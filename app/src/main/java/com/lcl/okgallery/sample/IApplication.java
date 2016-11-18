0baf90b413d2a77205d6a1af2e133b0d0cdc6dfe
w0080626 <w490576578@126.com>
2016-11-18 15:14:29 +0800

    initialize Commit.

---
+++ app/src/main/java/com/lcl/okgallery/sample/IApplication.java
@@ -0,0 +1,12 @@
+package com.lcl.okgallery.sample;
+
+import android.app.Application;
+
+/**
+ * Author:  Chenglong.Lu
+ * Email:   1053998178@qq.com | w490576578@gmail.com
+ * Date:    2016/11/18
+ * Description:
+ */
+public class IApplication extends Application {
+}
---
+++ okgallery/src/main/java/com/lcl/okgallery/OkGallery.java
@@ -0,0 +1,10 @@
+package com.lcl.okgallery;
+
+/**
+ * Author:  Chenglong.Lu
+ * Email:   1053998178@qq.com | w490576578@gmail.com
+ * Date:    2016/11/18
+ * Description:
+ */
+public class OkGallery {
+}
